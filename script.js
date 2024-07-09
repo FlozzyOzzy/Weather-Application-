9b37bcd91071030d06a3f7304d520bb91af126a0
Florence Macharia <floremarsh@gmail.com>
2024-07-09 11:46:32 +0300

    Inital Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const apiKey = "28d9a865a4a72821d217dd5d8c2141a3";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="; 
+
+  const searchBox = document.querySelector(".search input");
+  const searchBtn = document.querySelector(".search button");
+  const weatherIcon = document.querySelector(".weather-icon");
+
+  async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    if ( response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".Weather").style.display = "none";
+    }
+    else {
+        
+        var data = await response.json();
+
+
+document.querySelector(".city").innerText = data.name;
+document.querySelector(".temp").innerText = Math.round(data.main.temp) + "°C";
+document.querySelector(".humidity").innerText = data.main.humidity + "%";
+document.querySelector(".wind").innerText = data.wind.speed + "km/h";
+
+if (data.weather[0].main == "Clouds") {
+
+weatherIcon.src = "images/clouds.png";
+}  
+else if (data.weather[0].main == "Rain") {
+weatherIcon.src = "images/rain.png";
+}
+
+else if (data.weather[0].main == "Drizzle") {
+weatherIcon.src = "images/drizzle.png";
+}
+else if (data.weather[0].main == "Mist") {
+weatherIcon.src = "images/mist.png";
+}
+else if (data.weather[0].main == "Snow") {
+weatherIcon.src = "images/snow.png";
+}
+
+document.querySelector(".Weather").style.display = "block";
+document.querySelector(".error").style.display = "none";
+    }
+
+   }
+
+
+  searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+  });
